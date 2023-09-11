38e1a9e66f2fea3bd870c0b992c9ee9c18d30587
alejandro.noris <alejandro.noris@asey.gob.mx>
2023-09-11 12:05:51 -0600

    Corrección al peso de la página, se hicieron más ligeras las img

--- front-consultorio-pame/src/componentes/portada/index.js
+++ front-consultorio-pame/src/componentes/portada/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Portada = () => {
   return (
     <section id= 'Portada' className='frontpageContainer'>
-        <img className='bgFrontpage' src='Portada.png'/>
+        <img className='bgFrontpage' src='Portada.jpg'/>
     </section>
   )
 }
