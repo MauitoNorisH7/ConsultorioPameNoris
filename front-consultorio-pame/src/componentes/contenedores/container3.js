06e0671870c03eef8beccb2cd86031d12255f7e5
Mauricio Noris <mau-noris@hotmail.com>
2023-10-17 11:56:33 -0600

    Servicios con boton

--- front-consultorio-pame/src/componentes/contenedores/container3.js
+++ front-consultorio-pame/src/componentes/contenedores/container3.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const Container3 = () => {
   // Usamos un objeto para llevar el estado de múltiples elementos de texto
@@ -9,13 +9,15 @@ export const Container3 = () => {
     // Cambia el estado del texto específico basado en su ID
     setEstadosTexto((estadoPrevio) => ({
       ...estadoPrevio,
-      [id]: !estadoPrevio[id],  // Alterna entre mostrar/ocultar
+      [id]: !estadoPrevio[id], // Alterna entre mostrar/ocultar
     }));
   };
 
   return (
     <section id="Servicios" className="section-services">
-
+      <div className="brightBlueLine">
+        <h1>Servicios</h1>
+      </div>
       <div className="servicesGroup">
         {/* INICIO DEL SERVICIO 1 */}
         <div className="singleService">
@@ -23,12 +25,27 @@ export const Container3 = () => {
 
           <h2>Blanqueamiento dental</h2>
 
-          <div className={estadosTexto['servicio1'] ? "descriptionService text-container expanded" : " descriptionService text-container collapsed"}>
-            <p style={{display: estadosTexto['servicio1'] ? "block" : "none"}}>
-              Limpieza profunda para eliminar el sarro en dientes con un escariador de ultrasonido y posteriormente un pulido con pasta dental especial.
+          <div
+            className={
+              estadosTexto["servicio1"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio1"] ? "block" : "none" }}
+            >
+              Limpieza profunda para eliminar el sarro en dientes con un
+              escariador de ultrasonido y posteriormente un pulido con pasta
+              dental especial.
             </p>
           </div>
-          <i className={`bx bx-md ${estadosTexto['servicio1'] ? 'bx-minus-circle' : 'bx-plus-circle'} limpieza`} onClick={() => manejarClick('servicio1')}></i>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio1"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio1")}
+          ></i>
         </div>
         {/* FIN DEL SERVICIO 1 */}
 
@@ -38,15 +55,28 @@ export const Container3 = () => {
 
           <h2>Blanqueamiento dental</h2>
 
-          <div className={estadosTexto['servicio2'] ? "descriptionService text-container expanded" : " descriptionService text-container collapsed"}>
-            <p style={{display: estadosTexto['servicio2'] ? "block" : "none"}}>
+          <div
+            className={
+              estadosTexto["servicio2"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio2"] ? "block" : "none" }}
+            >
               Se puede elegir entre 2 métodos: el primero, en consultorio con
               una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
               es en casa, con guardas donde se aplica el tratamiento blanqueador
               por 7 días.
             </p>
           </div>
-          <i className={`bx bx-md ${estadosTexto['servicio2'] ? 'bx-minus-circle' : 'bx-plus-circle'} limpieza`} onClick={() => manejarClick('servicio2')}></i>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio2"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio2")}
+          ></i>
         </div>
 
         {/* FIN DEL SERVICIO 2 */}
@@ -56,14 +86,30 @@ export const Container3 = () => {
         <div className="singleService">
           <img className="imgService" src="Servicio-3.png" alt="Servicio 3" />
 
-          <h2>Prótesis removibles</h2>
+          <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
-              Prótesis dentales de “quitar y poner” para reponer brechas amplias
-              de piezas dentales perdidas.
+          <div
+            className={
+              estadosTexto["servicio3"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio3"] ? "block" : "none" }}
+            >
+              Se puede elegir entre 2 métodos: el primero, en consultorio con
+              una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
+              es en casa, con guardas donde se aplica el tratamiento blanqueador
+              por 7 días.
             </p>
           </div>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio3"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio3")}
+          ></i>
         </div>
 
         {/* FIN DEL SERVICIO 3 */}
@@ -71,17 +117,32 @@ export const Container3 = () => {
         {/* INICIO DEL SERVICIO 4 */}
 
         <div className="singleService">
-          <img className="imgService" src="Servicio-3.png" alt="Servicio 3" />
+          <img className="imgService" src="Servicio-3.png" alt="Servicio 4" />
 
-          <h2>Prótesis fija</h2>
+          <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
-              Coronas, puentes, carillas e incrustaciones que se colocan sobre
-              el diente para recuperar su tamaño, forma, resistencia, tamaño y
-              apariencia.
+          <div
+            className={
+              estadosTexto["servicio4"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio4"] ? "block" : "none" }}
+            >
+              Se puede elegir entre 2 métodos: el primero, en consultorio con
+              una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
+              es en casa, con guardas donde se aplica el tratamiento blanqueador
+              por 7 días.
             </p>
           </div>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio4"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio4")}
+          ></i>
         </div>
 
         {/* FIN DEL SERVICIO 4 */}
@@ -89,16 +150,32 @@ export const Container3 = () => {
         {/* INICIO DEL SERVICIO 5 */}
 
         <div className="singleService">
-          <img className="imgService" src="Servicio-3.png" alt="Servicio 3" />
+          <img className="imgService" src="Servicio-3.png" alt="Servicio 5" />
 
-          <h2>Extracciones dentales</h2>
+          <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
-              Extracción de muelas del juicio, dientes ampliamente dañados,
-              restos de dientes y con infección.
+          <div
+            className={
+              estadosTexto["servicio5"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio5"] ? "block" : "none" }}
+            >
+              Se puede elegir entre 2 métodos: el primero, en consultorio con
+              una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
+              es en casa, con guardas donde se aplica el tratamiento blanqueador
+              por 7 días.
             </p>
           </div>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio4"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio5")}
+          ></i>
         </div>
 
         {/* FIN DEL SERVICIO 5 */}
@@ -106,16 +183,32 @@ export const Container3 = () => {
         {/* INICIO DEL SERVICIO 6 */}
 
         <div className="singleService">
-          <img className="imgService" src="Servicio-3.png" alt="Servicio 3" />
+          <img className="imgService" src="Servicio-3.png" alt="Servicio 6" />
 
-          <h2>Endodoncia, periodoncia, rehabilitación de implantes</h2>
+          <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
-              Interdisciplinario con otros especialistas de la salud en la
-              clínica.
+          <div
+            className={
+              estadosTexto["servicio6"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio6"] ? "block" : "none" }}
+            >
+              Se puede elegir entre 2 métodos: el primero, en consultorio con
+              una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
+              es en casa, con guardas donde se aplica el tratamiento blanqueador
+              por 7 días.
             </p>
           </div>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio6"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio6")}
+          ></i>
         </div>
 
         {/* FIN DEL SERVICIO 6 */}
@@ -123,17 +216,32 @@ export const Container3 = () => {
         {/* INICIO DEL SERVICIO 7 */}
 
         <div className="singleService">
-          <img className="imgService" src="Servicio-3.png" alt="Servicio 3" />
+          <img className="imgService" src="Servicio-3.png" alt="Servicio 7" />
 
-          <h2>Resinas dentales (empastes):</h2>
+          <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
-              Proceso en el cual se elimina la caries existente en el diente
-              para reconstruir con resina del mismo color del diente dejándolo
-              de manera natural.
+          <div
+            className={
+              estadosTexto["servicio7"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio7"] ? "block" : "none" }}
+            >
+              Se puede elegir entre 2 métodos: el primero, en consultorio con
+              una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
+              es en casa, con guardas donde se aplica el tratamiento blanqueador
+              por 7 días.
             </p>
           </div>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio7"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio7")}
+          ></i>
         </div>
 
         {/* FIN DEL SERVICIO 7 */}
@@ -143,15 +251,30 @@ export const Container3 = () => {
         <div className="singleService">
           <img className="imgService" src="Servicio-3.png" alt="Servicio 3" />
 
-          <h2>Diagnóstico</h2>
+          <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
-              Cita en la cual se examina a profundidad el estado de los dientes
-              y boca del paciente, se elabora un diagnóstico, plan de
-              tratamiento y un presupuesto
+          <div
+            className={
+              estadosTexto["servicio8"]
+                ? "descriptionService text-container expanded"
+                : " descriptionService text-container collapsed"
+            }
+          >
+            <p
+              style={{ display: estadosTexto["servicio8"] ? "block" : "none" }}
+            >
+              Se puede elegir entre 2 métodos: el primero, en consultorio con
+              una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
+              es en casa, con guardas donde se aplica el tratamiento blanqueador
+              por 7 días.
             </p>
           </div>
+          <i
+            className={`bx bx-md ${
+              estadosTexto["servicio8"] ? "bx-minus-circle" : "bx-plus-circle"
+            } limpieza`}
+            onClick={() => manejarClick("servicio8")}
+          ></i>
         </div>
 
         {/* FIN DEL SERVICIO 8 */}
