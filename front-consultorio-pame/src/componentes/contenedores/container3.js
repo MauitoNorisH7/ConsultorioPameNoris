7d9580e8a95831dbb5ab276c7a18fa419973aa2e
Alexnoris <alex-noris@hotmail.com>
2023-10-16 20:36:20 -0600

    Se hicieron cambios para mostrar y cerrar la información en la sección de servicios

--- front-consultorio-pame/src/componentes/contenedores/container3.js
+++ front-consultorio-pame/src/componentes/contenedores/container3.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
 
 export const Container3 = () => {
+  // Usamos un objeto para llevar el estado de múltiples elementos de texto
+  const [estadosTexto, setEstadosTexto] = useState({});
+
+  // Esta función ahora acepta un ID para identificar qué ítem está siendo manipulado
+  const manejarClick = (id) => {
+    // Cambia el estado del texto específico basado en su ID
+    setEstadosTexto((estadoPrevio) => ({
+      ...estadoPrevio,
+      [id]: !estadoPrevio[id],  // Alterna entre mostrar/ocultar
+    }));
+  };
+
   return (
     <section id="Servicios" className="section-services">
-      <div className="brightBlueLine">
-        <h1>Servicios</h1>
-      </div>
 
       <div className="servicesGroup">
         {/* INICIO DEL SERVICIO 1 */}
         <div className="singleService">
-          <img className="imgService" src="Servicio-1.png" alt="Servicio 1" />
+          <img className="imgService" src="Servicio-2.png" alt="Servicio 2" />
 
-          <h2>Limpieza dental</h2>
+          <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
-              Limpieza profunda para eliminar el sarro en dientes con un
-              escariador de ultrasonido y posteriormente un pulido con pasta
-              dental especial.
+          <div className={estadosTexto['servicio1'] ? "descriptionService text-container expanded" : " descriptionService text-container collapsed"}>
+            <p style={{display: estadosTexto['servicio1'] ? "block" : "none"}}>
+              Limpieza profunda para eliminar el sarro en dientes con un escariador de ultrasonido y posteriormente un pulido con pasta dental especial.
             </p>
           </div>
+          <i className={`bx bx-md ${estadosTexto['servicio1'] ? 'bx-minus-circle' : 'bx-plus-circle'} limpieza`} onClick={() => manejarClick('servicio1')}></i>
         </div>
         {/* FIN DEL SERVICIO 1 */}
 
@@ -30,14 +38,15 @@ export const Container3 = () => {
 
           <h2>Blanqueamiento dental</h2>
 
-          <div className="descriptionService">
-            <p>
+          <div className={estadosTexto['servicio2'] ? "descriptionService text-container expanded" : " descriptionService text-container collapsed"}>
+            <p style={{display: estadosTexto['servicio2'] ? "block" : "none"}}>
               Se puede elegir entre 2 métodos: el primero, en consultorio con
               una duración de 1 a 2 hr aplicado por el odontólogo y el segundo,
               es en casa, con guardas donde se aplica el tratamiento blanqueador
               por 7 días.
             </p>
           </div>
+          <i className={`bx bx-md ${estadosTexto['servicio2'] ? 'bx-minus-circle' : 'bx-plus-circle'} limpieza`} onClick={() => manejarClick('servicio2')}></i>
         </div>
 
         {/* FIN DEL SERVICIO 2 */}
